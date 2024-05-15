49d97e945123641889fce47be64609fc2f0fe61e
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-15 09:10:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tbody = document.querySelector("tbody");
 const descItem = document.querySelector("#desc");
 const amount = document.querySelector("#amount");
-const type =
+const type = document.querySelector("#type");
