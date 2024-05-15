62460b387732ffa7c858eb6538bd1d5220fc8ad7
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-15 09:11:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const descItem = document.querySelector("#desc");
 const amount = document.querySelector("#amount");
 const type = document.querySelector("#type");
 const btnNew = document.querySelector("#btnNew");
+
+const incomes =
