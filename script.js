31331047dc1ff0cd3b9e8d1ae45c46178d353aeb
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-15 09:15:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,8 @@ function loadItens() {
 
   getTotals();
 }
+
+function getTotals() {
+  const amountIncomes = items
+    .filter((item) => item.type === "Entrada")
+    .map((transaction) => Number(transaction.amount));
